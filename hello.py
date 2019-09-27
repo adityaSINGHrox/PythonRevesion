1e72b428195693a21d9eda0f6f1b44927798d58b
Aditya Singh <addssinghrox@gmail.com>
2019-09-28 02:01:10 +0530

    lets c strings

--- hello.py
+++ hello.py
@@ -1,37 +1,26 @@
+#########
+# lets checkout on strings
+
 import os
 os.system('clear')
-print('')
-print('hello world')
+# we need to print like "GET BACK TO WORK"
 
-#trying out oop
-class Person:
-
-    def __init__(self,initialAge):
-        if initialAge < 0:
-            print('Age is not valid, setting age to 0.')
-            self.intitialAge = 0
-        else :
-            self.initialAge = initialAge
-        # Add some more code to run some checks on initialAge
-    def amIOld(self):
-        if self.initialAge in range(0,13):
-            print('You are young.')
-        if self.initialAge in range(13,18):
-            print('You are a teenager')
-        if self.initialAge >18:
-            print('You are old')
-        # Do some computations in here and print out the correct statement to the console
-    def yearPasses(self):
-        self.initialAge += 1
-        print(self.initialAge)
-        # Increment the age of the person in here
-
-t = int(input())
-for i in range(0, t):
-    age = int(input())
-    p = Person(age)
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()
-    p.amIOld()
-    print("")
+greetings = 'my boss yelled "GET BACK TO WORK"'
+# using esczpe characters like
+yo  = 'hey wassup \"FNIE BRO\"'
+print(greetings)
+print(yo)
+#use \n for new line
+#############
+#concatenation
+adi = "yo Man wassup"
+singh = " cool Man"
+print((adi+singh).upper())
+print((adi+singh).lower())
+print((adi+singh).capitalize())#starting alphabet
+print((adi+singh).title())
+print((adi+singh).swapcase())
+print(len(adi+singh))
+print(greetings[10:15])
+print(greetings.split(" ")[5])#seperate wrt " "
+#['my', 'boss', 'yelled', '"GET', 'BACK', 'TO', 'WORK"']
